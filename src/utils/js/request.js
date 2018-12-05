266b85ec9130b543f671b96c9f3c0c097822e09c
xxj95719 <87416959@qq.com>
2018-12-05 17:55:49 +0800

    :wrench:修改axios配置

--- src/utils/js/request.js
+++ src/utils/js/request.js
@@ -1,63 +1,61 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_PATH = 'https://api.com/'
-axios.defaults.withCredentials = true
+const BASE_PATH = "https://api.com/";
+axios.defaults.withCredentials = true;
 // Add a request interceptor
 axios.interceptors.request.use(
   function(config) {
     // Do something before request is sent
-    return config
+    return config;
   },
   function(error) {
     // Do something with request error
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 // Add a response interceptor
 axios.interceptors.response.use(
   function(response) {
     // Do something with response data
-    return response
+    return response;
   },
   function(error) {
     // Do something with response error
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 const request = (
-  { url = '/', data = {}, type = 'get' } = {},
+  { url = "/", data = {}, type = "get" } = {},
   otherConfig = {}
 ) => {
-  let isFormData = data instanceof FormData
-  url = /http/.test(url) ? url : `${BASE_PATH}${url}`
+  let isFormData = data instanceof FormData;
+  url = /http/.test(url) ? url : `${BASE_PATH}${url}`;
   let config = {
     method: type,
     url,
-    [type === 'get' ? 'params' : 'data']:
-      type === 'get' ? data : isFormData ? data : formatData(data),
+    [type === "get" ? "params" : "data"]:
+      type === "get" ? data : isFormData ? data : formatData(data),
     headers: {
-      'Content-Type': isFormData
-        ? ' multipart/form-data'
-        : 'application/x-www-form-urlencoded'
+      "Content-Type": isFormData
+        ? " multipart/form-data"
+        : "application/x-www-form-urlencoded"
     },
     ...otherConfig
-  }
+  };
 
-  Object.assign(config, otherConfig)
-  return axios(config)
-}
+  Object.assign(config, otherConfig);
+  return axios(config);
+};
 
 function formatData(data) {
   // Do whatever you want to transform the data
-  let ret = ''
+  let ret = "";
   for (let prop in data) {
-    if (data[prop] != null && data[prop] != undefined && data[prop] !== '') {
-      ret += `${ret ? '&' : ''}${encodeURIComponent(prop)}=${encodeURIComponent(
-        data[prop]
-      )}`
-    }
+    ret += `${ret ? "&" : ""}${encodeURIComponent(prop)}=${encodeURIComponent(
+      data[prop] === null ? "" : data[prop]
+    )}`;
   }
-  return ret
+  return ret;
 }
-export default request
+export default request;
