d4e7bb46c692a46c13ec0ff2090ab6f2eac5b06a
xxj95719 <87416959@qq.com>
2019-02-12 15:25:23 +0800

    💩

--- historyServe.js
+++ historyServe.js
@@ -1,22 +1,23 @@
 // 模拟生产路径
-var express = require('express');
+var express = require("express");
 var app = express();
-var history = require('connect-history-api-fallback');
-app.use(history(
-  {
-    htmlAcceptHeaders: ['textml', 'application/xhtml+xml'],
+var history = require("connect-history-api-fallback");
+app.use(
+  history({
+    htmlAcceptHeaders: ["textml", "application/xhtml+xml"],
     rewrites: [
       {
         from: /^\/aa\/bb\/(?!static)/,
         to: function(context) {
-          return '/aa/bb/index.html' ;
+          return "/aa/bb/index.html";
         }
       }
     ]
-  }
-));
+  })
+);
 
-app.listen(3003, function () {
-  console.log('server started at listening on: localhost:3000/aa/bb/,please enter ctrl^c exit');
+app.listen(3003, function() {
+  console.log(
+    "server started at listening on: localhost:3000/aa/bb/,please enter ctrl^c exit"
+  );
 });
-app.use('/nsjw/claimsArrow',express.static('./dist'));
